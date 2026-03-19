5b50324a9c8281742589fa66e5a38620e5e24c0d
Eduardo Geronimo <ednirvana18@gmail.com>
2026-03-19 18:13:45 -0500

    Agregando Dockerfile

--- obj/Debug/net8.0/ServiceTowerWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServiceTowerWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceTowerWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0725f50160d25d756cb6753bb1cd43295b51734")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceTowerWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceTowerWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
